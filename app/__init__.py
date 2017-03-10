620d7cd6e7c6685df00290d1ae7182bd90ed7e17
Erica Roy <digitalnico@icloud.com>
2017-03-10 16:57:51 -0600

    Added boilerplate submission

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect
 from app.forms.orgForm import OrgForm
-
 
 
 app = Flask(__name__, instance_relative_config=True)
 
 
 app.config.from_object('config')
-#app.config.from_pyfile('config.py')
+app.config.from_pyfile('config.py')
 
 
 @app.route('/')
@@ -18,9 +17,17 @@ def index():
 @app.route('/create/', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
-        form = OrgForm(request.form)
+        form = OrgForm(request.form, csrf_enabled=False)
+
+        if form.validate_on_submit():
+            flash('You have successfully created an organization', 'success')
+            getTitle = form.title.data
+            netID = form.netID.data
+            print(getTitle)
+            print(netID)
+
+            return redirect('/')
 
     else:
-
-        form = OrgForm(csrf_enabled = False)
+        form = OrgForm(csrf_enabled=False)
     return render_template('create.html', form=form)
