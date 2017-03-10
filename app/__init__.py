d42cbc2d58d112214c013045ebde2d1fb17892a7
Erica Roy <digitalnico@icloud.com>
2017-03-10 15:19:18 -0600

    Added html structure base

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
-from flask import Flask
-from app import views
+from flask import Flask, render_template
+
 
 app = Flask(__name__, instance_relative_config=True)
 
 
 app.config.from_object('config')
 #app.config.from_pyfile('config.py')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
