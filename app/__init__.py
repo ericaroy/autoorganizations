69ca94a0f643491091bc9dcb9c2d54c9bc8159b2
Erica Roy <digitalnico@icloud.com>
2017-04-04 12:10:37 -0500

    testing cas

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 import os
 import sys
 import logging
+import flask
+import urllib
+from urllib.parse import urlparse
+import argparse
 from app.forms.orgForm import OrgForm
 from app.auth import get_token
 from app.organizations import createOrganization, check_netid
@@ -17,7 +21,7 @@ cas = CAS(app)
 app.config.from_object('config')
 app.secret_key = os.environ['SECRET_KEY']
 app.config['CAS_SERVER'] = 'https://netid.test.ualr.edu'
-app.config['CAS_AFTER_LOGIN'] = 'create'
+app.config['CAS_AFTER_LOGIN'] = 'archive'
 
 
 @app.route('/')
@@ -44,12 +48,64 @@ def create():
     return render_template('create.html', form=form)
 
 
-@app.route('/archive/')
-@login_required
-def archive():
-    return render_template('archive.html', username=cas.username)
+@app.route('/login/')
+def route_login():
+
+    if 'ticket' in Flask:
+        flask.session['_cas_token'] = flask.request.args['ticket']
+
+    if '_cas_token' in flask.session:
+
+        if validate(flask.session['_cas_token']):
+            redirect_url = flask.url_for('create')
+        else:
+            redirect_url = create_cas_login_url(app.config['cas_server'])
+            del flask.session['_cas_token']
+    else:
+        redirect_url = create_cas_login_url(app.config['cas_server'])
+
+    app.logger.debug('Redirecting to: {}'.format(redirect_url))
+
+    return flask.redirect(redirect_url)
+
+def create_cas_login_url(cas_url):
+    service_url = urllib.quote(
+        flask.url_for('route_login',_external=True))
+    return urlparse.urljoin(
+        cas_url,
+        '/cas/?service={}'.format(service_url))
 
 
+def validate(ticket):
 
+    app.logger.debug("validating token {}".format(ticket))
 
+    cas_validate_url = create_cas_validate_url(
+        app.config['cas_server'], ticket)
 
+    app.logger.debug("Making GET request to {}".format(
+        cas_validate_url))
+
+    try:
+        (isValid, username) = urllib.urlopen(cas_validate_url).readlines()
+        isValid = True if isValid.strip() == 'yes' else False
+        username = username.strip()
+    except ValueError:
+        app.logger.error("CAS returned unexpected result")
+        isValid = False
+
+    if isValid:
+        app.logger.debug("valid")
+        flask.session['username'] = username
+    else:
+        app.logger.debug("invalid")
+
+    return isValid
+
+def create_cas_validate_url(cas_url, ticket):
+    service_url = urllib.quote(
+        flask.url_for('route_login',_external=True))
+    ticket = urllib.quote(ticket)
+    return urlparse.urljoin(
+        cas_url,
+        '/cas/validate?service={}&ticket={}'.format(service_url, ticket))
